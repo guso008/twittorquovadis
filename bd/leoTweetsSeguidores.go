ae99a8c60aa5a8ca17f51292ba54a7c37835f1da
Guso Olivares <guso008@gmail.com>
2021-08-22 16:24:06 -0300

    Changes varius

--- bd/leoTweetsSeguidores.go
+++ bd/leoTweetsSeguidores.go
@@ -23,22 +23,20 @@ func LeoTweetsSeguidores(ID string, pagina int) ([]models.DevuelvoTweetsSeguidor
 	condiciones = append(condiciones, bson.M{
 		"$lookup": bson.M{
 			"from":         "tweet",
-			"localfield":   "usuariorelacionid",
-			"foreingField": "userid",
+			"localField":   "usuariorelacionid",
+			"foreignField": "userid",
 			"as":           "tweet",
 		}})
 	condiciones = append(condiciones, bson.M{"$unwind": "$tweet"})
-	condiciones = append(condiciones, bson.M{"$sort": bson.M{"fecha": -1}})
+	condiciones = append(condiciones, bson.M{"$sort": bson.M{"tweet.fecha": -1}})
 	condiciones = append(condiciones, bson.M{"$skip": skip})
 	condiciones = append(condiciones, bson.M{"$limit": 20})
 
 	cursor, err := col.Aggregate(ctx, condiciones)
 	var result []models.DevuelvoTweetsSeguidores
 	err = cursor.All(ctx, &result)
-
 	if err != nil {
 		return result, false
 	}
-
 	return result, true
 }
--- models/devuelvoTweetsSeguidores.go
+++ models/devuelvoTweetsSeguidores.go
@@ -8,12 +8,12 @@ import (
 
 /*DevuelvoTweetsSeguidores es la estructura con la que devolvemos los tweets*/
 type DevuelvoTweetsSeguidores struct {
-	ID                primitive.ObjectID `bson:"_id" json:"_id, omitempty"`
-	UsuarioID         string             `bson:"usuarioid" json:"userId, omitempty"`
-	UsuarioRelacionID string             `bson:"usuariorelacionid" json:"userRelationId, omitempty"`
+	ID                primitive.ObjectID `bson:"_id" json:"_id,omitempty"`
+	UsuarioID         string             `bson:"usuarioid" json:"userId,omitempty"`
+	UsuarioRelacionID string             `bson:"usuariorelacionid" json:"userRelationId,omitempty"`
 	Tweet             struct {
-		Mensaje string    `bson:"mensaje" json:"mensaje, omitempty"`
-		Fecha   time.Time `bson:"fecha" json:"fecha, omitempty"`
-		ID      string    `bson:"_id" json:"_id, omitempty"`
+		Mensaje string    `bson:"mensaje" json:"mensaje,omitempty"`
+		Fecha   time.Time `bson:"fecha" json:"fecha,omitempty"`
+		ID      string    `bson:"_id" json:"_id,omitempty"`
 	}
 }
